98d18733103c612aa4085dac2c93840b824d8ce5
Chad Crawford <chad@cacrawford.org>
2018-11-19 23:16:18 -0600

    Added fully implemented Wumpus MDP.

--- wumpus.py
+++ wumpus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from enum import Enum
-from mdp import MDP
+from mdp import FiniteStateMDP, MDPState
 import itertools
 import numpy as np
 
@@ -23,41 +23,59 @@ _TXT = {
 }
 
 
-_UP = np.array([0, -1])
-_DOWN = np.array([0, 1])
+_UP = np.array([0, 1])
+_DOWN = np.array([0, -1])
 _LEFT = np.array([-1, 0])
 _RIGHT = np.array([1, 0])
 
 
-class WumpusState:
-    def __init__(self, x, y, has_gold, has_immunity):
+class WumpusState(MDPState):
+    def __init__(self, x, y, has_gold, has_immunity, width, height):
         self.x = x
         self.y = y
         self.has_gold = has_gold
         self.has_immunity = has_immunity
+        self._width = width
+        self._height = height
+
+    def clone(self):
+        return WumpusState(self.x, self.y, self.has_gold, self.has_immunity, self._width, self._height)
 
     @property
     def pos(self):
         return np.array([self.x, self.y])
 
+    @property
+    def i(self):
+        g = 2 if self.has_gold else 0
+        i = 1 if self.has_immunity else 0
+        return (self._height * self.x + self.y) * (g+i)
+
+    def __repr__(self):
+        return 'pos: ({x}, {y}) has_gold: {has_gold} has_immunity: {has_immunity}'.format(**self.__dict__)
+
 
 def _clip(p, max_x, max_y):
-    p = np.array(max(min(p[0], max_x), 1),
-                 max(min(p[0], max_y), 1))
+    p = np.array([max(min(p[0], max_x), 1),
+                  max(min(p[1], max_y), 1)])
+    return p
 
 
-class WumpusMDP(MDP):
-    def __init__(self, w, h, cost):
+_OBS_KEYS = ['wumpus', 'pit', 'goal']
+_OBS_REWARDS = {
+    'wumpus': -5.0,
+    'pit': -1.0,
+    'goal': 10.0
+}
+_OBJ_KEYS = ['gold', 'immune']
+class WumpusMDP(FiniteStateMDP):
+    def __init__(self, w, h, move_cost=-0.1, gold_reward=10):
         self._w = w
         self._h = h
-        self._cost = cost
-        self._objs = {
-            'pit': {},
-            'gold': {},
-            'immune': {},
-            'goal': {},
-            'wumpus': {}
-        }
+        self.move_cost = move_cost
+        self.gold_reward = gold_reward
+        self._obs = {k:{} for k in _OBS_KEYS}
+        self._obj = {k:set([]) for k in _OBJ_KEYS}
 
     @property
     def width(self):
@@ -68,6 +86,10 @@ class WumpusMDP(MDP):
         return self._h
 
     @property
+    def num_states(self):
+        return self.width * self.height * 4
+
+    @property
     def states(self):
         return itertools.product(
             range(self.width), range(self.height), (True, False), (True, False))
@@ -76,25 +98,57 @@ class WumpusMDP(MDP):
     def actions(self):
         return Actions
 
+    @property
+    def initial_state(self):
+        return WumpusState(0, 0, False, False, self.width, self.height)
+
     def actions_at(self, state):
         a = [Actions.LEFT, Actions.RIGHT, Actions.UP, Actions.DOWN]
-        if self.gold_at(state) or self.immunity_at(state):
+        if self.obj_at('gold', state.pos) or self.obj_at('immune', state.pos):
             a += [Actions.PICK_UP]
         return a
 
-    def act(self, state, action):
+    def p(self, state, action):
         if action in [Actions.PICK_UP]:
             return self.pick_up(state, action)
-        elif action in [Actions.UP, Actions.DOWN, Actions.LEFT, Actions.RIGHT]
+        elif action in [Actions.UP, Actions.DOWN, Actions.LEFT, Actions.RIGHT]:
             return self.move(state, action)
         else:
             raise Exception("Invalid action specified: {}".format(action))
 
-    def has_at(self, kind, pos):
-        return tuple(pos) in self._objs[kind].keys()
+    def r(self, s1, s2):
+        ## if it's the pit, return the default pit bad reward
+        if self.obs_at('pit', s2.pos):
+            return self._obs['pit'][tuple(s2.pos)]
 
-    def reward_at(self, kind, pos):
-        return self._objs[kind][tuple(pos)]
+        ## if it's the goal, return the goal reward + gold reward if applicable
+        if self.obs_at('goal', s2.pos):
+            gr = self.gold_reward if s2.has_gold else 0
+            return self._obs['goal'][tuple(s2.pos)] + gr
+
+        ## if it's the wumpus, return the negative wumpus reward unless player has immunity
+        if self.obs_at('wumpus', s2.pos) and not s2.has_immunity:
+            return self._obs['wumpus'][tuple(s2.pos)]
+
+        ## otherwise return the move cost
+        return self.move_cost
+
+    def is_terminal(self, state):
+        ## if we're at the wumpus and have no immunity, we die
+        if self.obs_at('wumpus', state.pos) and not state.has_immunity:
+            return True
+
+        ## if we're at the goal, we win
+        if self.obs_at('goal', state.pos):
+            return True
+
+        return False
+
+    def obs_at(self, kind, pos):
+        return tuple(pos) in self._obs[kind].keys()
+
+    def obj_at(self, kind, pos):
+        return tuple(pos) in self._obj[kind]
 
     def move(self, state, action):
         probs = [0.7, 0.15, 0.15]
@@ -108,123 +162,49 @@ class WumpusMDP(MDP):
         elif action == Actions.RIGHT:
             alst = [_RIGHT, _DOWN, _UP]
 
-        new_p = _clip(state.pos + np.random.choice(alst, p=probs),
-                      self.width, self.height)
-        new_state = WumpusState(new_p[0], new_p[1], state.has_gold, state.has_immunity)
+        x = []
+        for a in alst:
+            new_state = state.clone()
+            new_pos = _clip(state.pos + a, self.width, self.height)
+            new_state.x = new_pos[0]
+            new_state.y = new_pos[1]
+            x += [new_state]
 
-        reward = -self._cost
-        is_terminal = False
-        if state.has_gold and self.has_at('goal', new_state.pos):
-            is_terminal = True
-            reward = self.reward_at('goal', new_state.pos)
-        elif not state.has_immunity and self.has_at('wumpus', new_state.pos):
-            is_terminal = True
-            reward = self.reward_at('wumpus', new_state.pos)
-        return new_state
+        return zip(x, probs)
 
     def pick_up(self, state, action):
-        has_gold = state.has_gold
-        has_immunity = state.has_immunity
-        if self.has_at('immunity', new_state.pos):
-            pass
+        new_state = state.clone()
+        if self.obj_at('gold', state.pos):
+            new_state.has_gold = True
+        if self.obj_at('immune', state.pos):
+            new_state.has_immunity = True
+        return [(new_state, 1.0)]
 
-#     def __is_valid_state(self, s):
-#         return True
-#     @property
-#     def states(self):
-#         return filter(lambda x: self.__is_valid_state(x),
-#                       itertools.product(range(1, self.__w+1), range(1, self.__h+1), range(2), range(2)))
-#     def actions(self, s):
-#         if self.is_gold(s) or self.is_immunity(s):
-#             return ["left", "right", "up", "down", "pick up"]
-#         else:
-#             return ["left", "right", "up", "down"]
-#     def is_terminal(self, s):
-#         if self.is_goal(s) and self.has_gold(s):
-#             return True
-#         elif self.is_wumpus(s) and not self.has_immunity(s):
-#             return True
-#         else:
-#             return False
-#     def r(self, s, a=None):
-#         if self.is_gold(s) and a == "pick up":
-#             return self.__golds[s[:2]]
-#         if self.is_pit(s):
-#             return self.__pits[s[:2]]
-#         if self.is_wumpus(s) and not self.has_immunity(s):
-#             return self.__wumpus[s[:2]]
-#         if self.is_goal(s) and self.has_gold(s):
-#             return self.__goals[s[:2]]
-#         # if self.is_immunity(s):
-#         #     return self.__immunes[s[:2]]
-#         return self.__cost
-#     def p(self, s1, a, s2):
-#         if a == "pick up" and s2 == self.move(s1, a):
-#             return 1
-#         else:
-#             s3s = self.umove(self, s1, a)
-#             if s2 in s3s:
-#                 return \
-#                     (0.8 if s3s[0] == s2 else 0)\
-#                     + (0.1 if s3s[1] == s2 else 0)\
-#                     + (0.1 if s3s[2] == s2 else 0)
-#             else:
-#                 return 0
-#     def ps(self, s1, a):
-#         if a == "pick up":
-#             return [(self.move(s1, a), 1)]
-#         else:
-#             s2s = self.umove(s1, a)
-#             return [(s2s[0], 0.8), (s2s[1], 0.1), (s2s[2], 0.1)]
-#     def is_pit(self, s):
-#         return s[:2] in self.__pits
-#     def add_pit(self, x, r):
-#         self.__pits[x] = r
-#     def is_gold(self, s):
-#         return not self.has_gold(s) and s[:2] in self.__golds
-#     def add_gold(self, x, r):
-#         self.__golds[x] = r
-#     def is_immunity(self, s):
-#         return not self.has_immunity(s) and s[:2] in self.__immunes
-#     def add_immunity(self, x):
-#         self.__immunes[x] = 1
-#     def is_goal(self, s):
-#         return s[:2] in self.__goals
-#     def add_goal(self, x, r):
-#         self.__goals[x] = r
-#     def is_wumpus(self, s):
-#         return s[:2] in self.__wumpus
-#     def add_wumpus(self, x, r):
-#         self.__wumpus[x] = r
-#     def has_gold(self, s):
-#         return s[2]
-#     def has_immunity(self, s):
-#         return s[3]
-#     def move(self, s, a):
-#         s2 = []
-#         if a == "pick up" and self.is_gold(s):
-#             s2 = (s[0], s[1], 1, s[3])
-#         elif a == "pick up" and self.is_immunity(s):
-#             s2 = (s[0], s[1], s[2], 1)
-#         elif a == "left" and s[0] > 1:
-#             s2 = (s[0]-1, s[1], s[2], s[3])
-#         elif a == "right" and s[0] < self.__w:
-#             s2 = (s[0]+1, s[1], s[2], s[3])
-#         elif a == "up" and s[1] < self.__h:
-#             s2 = (s[0], s[1]+1, s[2], s[3])
-#         elif a == "down" and s[1] > 1:
-#             s2 = (s[0], s[1]-1, s[2], s[3])
-#         if not s2:
-#             return s
-#         else:
-#             return s2
-#     def umove(self, s, a):
-#         # pu = ["pick up"] if self.is_gold(s) or self.is_immunity(s) else []
-#         if a == "left":
-#             return map(lambda a2: self.move(s, a2), ["left", "up", "down"])
-#         elif a == "right":
-#             return map(lambda a2: self.move(s, a2), ["right", "up", "down"])
-#         elif a == "up":
-#             return map(lambda a2: self.move(s, a2), ["up", "left", "right"])
-#         else:
-#             return map(lambda a2: self.move(s, a2), ["down", "left", "right"])
+    def add_obstacle(self, kind, pos, reward=None):
+        ## default rewards
+        if not reward:
+            reward = _OBS_REWARDS[kind]
+        self._obs[kind][tuple(pos)] = reward
+
+    def add_object(self, kind, pos):
+        self._obj[kind].add(tuple(pos))
+
+    def display(self):
+        obs_lab = lambda p, lab, kind: lab if self.obs_at(kind, p) else ' '
+        obj_lab = lambda p, lab, kind: lab if self.obj_at(kind, p) else ' '
+        print('      ', end='')
+        for i in range(self.width):
+            print(' {:5d} '.format(i),end='')
+        print()
+        for j in reversed(range(self.height)):
+            print('{:5d} '.format(j), end='')
+            for i in range(self.width):
+                p = tuple([i, j])
+                l_s = 'S' if p == (0, 0) else ' '
+                l_w = obs_lab(p, 'W', 'wumpus')
+                l_p = obs_lab(p, 'P', 'pit')
+                l_gl = obs_lab(p, 'X', 'goal')
+                l_gd = obj_lab(p, 'G', 'gold')
+                l_i = obj_lab(p, 'I', 'immune')
+                print('|' + l_s+l_w+l_p+l_gl+l_gd+l_i,end='')
+            print('|')
